88e4a2c6ddbbfbb6339e4cca974cb704ab7f5938
Koldmunch <ChristofferMunch@Christoffers-MacBook-Pro.local>
2014-09-29 13:58:54 +0200

    Fiskkk

--- src/Fisk.java
+++ src/Fisk.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
 public class Fisk {
 
-	//Lad os se om det virker
+	public static void main (String args []) {
 	
-	//Det skide godt
+		
+	Scanner user_input = new Scanner( System.in );
 	
+	
+	String first_name;
+	System.out.print("Enter your first name: ");
+	first_name = user_input.next( );
+	
+	String family_name;
+	System.out.print("Enter your family name: ");
+	family_name = user_input.next( );
+	
+	String full_name;
+	full_name = first_name + " " + family_name;
+
+	System.out.println("You are " + full_name);
+	
+	
+}
 }
